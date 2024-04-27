4644b286099123b73fe0fc58635210a1aa472263
pawanyadavmamaindla <145900443+pawanyadavmamaindla@users.noreply.github.com>
2024-04-27 22:59:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
 app.use(require('./routes/user'))
 
+app.get('/', (req, res) => {
+    res.send("hello")
+})
+
 
     app.use(express.static('client/build'))
     const path = require('path')
